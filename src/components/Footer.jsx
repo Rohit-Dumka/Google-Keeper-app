c1d30bc3e54dbc8ca5c93551e7eb57d054209f05
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2022-06-06 22:17:16 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+    //copyright footer with year stamp on it
+  );
+}
+
+export default Footer;
